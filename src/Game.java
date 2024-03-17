52a6ee05e86f5005f40a32be1276915b019a2614
zyte09 <allen.aranez32@gmail.com>
2024-03-17 20:46:29 +0800

    added button

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+
 public class Game {
     JFrame window;
     Container container;
-    JPanel titleNamePanel;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    JButton startButton;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font buttonFont = new Font("Times New Roman", Font.PLAIN, 40);
+
+
     public static void main(String[] args) {
         new Game();
     }
@@ -19,9 +26,26 @@ public class Game {
 
         titleNamePanel = new JPanel();
         titleNamePanel.setBounds(100, 100, 600, 150);
-        titleNamePanel.setLayout(null);
-        titleNamePanel.setBackground(Color.blue);
+        titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("GAME");
+        titleNameLabel.setForeground(Color.WHITE);
+        titleNameLabel.setFont(titleFont);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("START");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(buttonFont);
+        startButton.setFocusable(false);
+
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+
         container.add(titleNamePanel);
+        container.add(startButtonPanel);
 
 
     }
