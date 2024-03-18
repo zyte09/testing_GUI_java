f8d0f45cdf1bb07beac5b4ad11af56c3a8fe4a3d
zyte09 <allen.aranez32@gmail.com>
2024-03-18 19:45:22 +0800

    added GameScreen

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
     JFrame window;
     Container container;
-    JPanel titleNamePanel, startButtonPanel;
+    JPanel titleNamePanel, startButtonPanel, mainTextPanel;
     JLabel titleNameLabel;
     JButton startButton;
-    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 75);
     Font buttonFont = new Font("Times New Roman", Font.PLAIN, 40);
-
+    JTextArea mainTextArea;
+    TitleScreenHandler tsHandler = new TitleScreenHandler();
 
     public static void main(String[] args) {
         new Game();
@@ -17,22 +20,23 @@ public class Game {
 
     public Game() {
         window = new JFrame();
-        window.setSize(800, 600);
+        window.setSize(1360, 768);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
         window.setVisible(true);
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
         container = window.getContentPane();
 
         titleNamePanel = new JPanel();
-        titleNamePanel.setBounds(100, 100, 600, 150);
+        titleNamePanel.setBounds(380, 189, 600, 150);
         titleNamePanel.setBackground(Color.black);
-        titleNameLabel = new JLabel("GAME");
+        titleNameLabel = new JLabel("Adeptus Tetris Trial");
         titleNameLabel.setForeground(Color.WHITE);
         titleNameLabel.setFont(titleFont);
 
         startButtonPanel = new JPanel();
-        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBounds(580, 459, 200, 100);
         startButtonPanel.setBackground(Color.black);
 
         startButton = new JButton("START");
@@ -40,13 +44,39 @@ public class Game {
         startButton.setForeground(Color.white);
         startButton.setFont(buttonFont);
         startButton.setFocusable(false);
+        startButton.addActionListener(tsHandler);
 
         titleNamePanel.add(titleNameLabel);
         startButtonPanel.add(startButton);
 
         container.add(titleNamePanel);
         container.add(startButtonPanel);
+    }
+    
+    public void GameScreen(){
+        titleNamePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.blue);
+        container.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("Main Text Area");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(buttonFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
 
 
+
+    }
+
+    public class TitleScreenHandler implements ActionListener {
+        public void actionPerformed(ActionEvent event){
+            GameScreen();
+        }
     }
 }
